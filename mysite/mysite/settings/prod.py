2b66927da52580e615eeeefc2934f1336199ab5f
Michael Wittig <witmic1@gmail.com>
2016-02-02 21:21:45 +0100

    configuring static root for production

--- mysite/mysite/settings/prod.py
+++ mysite/mysite/settings/prod.py
@@ -5,7 +5,8 @@ from .common import *
 DEBUG = False
 
 ALLOWED_HOSTS = [ 'django.sh4ke.acrux.uberspace.de' ]
-
 CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
+
+STATIC_ROOT = '/var/www/virtual/sh4ke/django.sh4ke.acrux.uberspace.de/static'
 
 USE_X_FORWARDED_HOST = True
