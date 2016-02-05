0b68e4baf5bf227d6a45f88a8b7fda5d6a260f27
Michael Wittig <witmic1@gmail.com>
2016-02-05 13:29:41 +0100

    adding blog template for posts

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from django.views.generic import ListView
+
 from blog.models import Post
+from . import views
 
 urlpatterns = [
     url(r'^$', ListView.as_view(
         queryset=Post.objects.all().order_by("-date")[:25],
         template_name="blog/blog.html")),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='post'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from .models import Post
+
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Post.objects.filter(date__lte=timezone.now())
